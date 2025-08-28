68f0011580cc8d469bdbae9fb34d0a32673bb165
Mariano Galiana <marianogaliana@gmail.com>
2025-08-28 01:12:20 -0300

    add error handling

--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,7 @@
 using System.Text;
 using API.Data;
 using API.Interfaces;
+using API.Middleware;
 using API.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
@@ -33,6 +34,7 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJw
 //builder.Services.AddOpenApi();  //Learn about configuring OpenAPI at https://aka.ms/aspnet/openapi
 
 var app = builder.Build();
+app.UseMiddleware<ExceptionMiddleware>();
 app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200","https://localhost:4200"));
 app.UseAuthentication(); //who are you?
 app.UseAuthorization();  //what are you allowed to do?
